150a1c4ff005d8ab75047d477625491b27f50222
Harry <harrybest800@gmail.com>
2021-03-17 19:45:35 +0000

    linked influxdb to bridge

--- server/app.js
+++ server/app.js
@@ -2,9 +2,26 @@
 const BEDROOM_SHELF_TEMPERATURE_TOPIC = "bedroom/shelf/temperature"
 const BEDROOM_SHELF_RELATIVE_HUMIDITY_TOPIC = "bedroom/shelf/relative_humidity"
 
+// setup mqtt
 var mqtt = require('mqtt')
-
 var client = mqtt.connect('mqtt://mosquitto') // should connect to the mosquitto docker container
+
+// setup influx db
+const {InfluxDB} = require('@influxdata/influxdb-client')
+
+// You can generate a Token from the "Tokens Tab" in the UI
+const token = '0gKXk00l36A4E9djn6Z0BcYv77oJw5WkvcZlZHBwyPrzsX12YUdRnY0J_y5krbF1BcK5NkjasCIhHq_f04nVYw==' // should probably change this to be an environment variable?
+const org = 'seabed'
+const bucket = 'ocean'
+
+const influxClient = new InfluxDB({url: 'http://influxdb:8086', token: token})
+
+const {Point} = require('@influxdata/influxdb-client')
+const writeApi = influxClient.getWriteApi(org, bucket)
+writeApi.useDefaultTags({host: 'host1', location:'bedroom_shelf'})
+
+
+
 
 client.on('connect', () => {
     console.log("connected to mqtt bridge")
@@ -14,11 +31,21 @@ client.on('connect', () => {
 })
 
 client.on('message', (topic, message) => {
+
+    let point = new Point('indoor-weather')
+    
     if (topic == BEDROOM_SHELF_TEMPERATURE_TOPIC) {
         console.log("temperature: " + message)
+
+        point = point.floatField("temperature", parseFloat(message))
+        
     }
     else if (topic == BEDROOM_SHELF_RELATIVE_HUMIDITY_TOPIC) {
         console.log("humidity: " + message)
+        point = point.floatField("relative_humidity", parseFloat(message))
     }
+
+    writeApi.writePoint(point)
+    
 
 })
