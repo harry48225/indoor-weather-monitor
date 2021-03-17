87d6d614b88105b40e8ae79d73cb9a087d579c81
Harry <harrybest800@gmail.com>
2021-03-17 17:23:32 +0000

    added mqtt support to temperature bridge

--- server/app.js
+++ server/app.js
@@ -1,6 +1,24 @@
-const express = require('express');
-const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`listening on port ${port}`))
+const BEDROOM_SHELF_TEMPERATURE_TOPIC = "bedroom/shelf/temperature"
+const BEDROOM_SHELF_RELATIVE_HUMIDITY_TOPIC = "bedroom/shelf/relative_humidity"
+
+var mqtt = require('mqtt')
+
+var client = mqtt.connect('mqtt://mosquitto') // should connect to the mosquitto docker container
+
+client.on('connect', () => {
+    console.log("connected to mqtt bridge")
+    
+    client.subscribe(BEDROOM_SHELF_TEMPERATURE_TOPIC)
+    client.subscribe(BEDROOM_SHELF_RELATIVE_HUMIDITY_TOPIC)
+})
+
+client.on('message', (topic, message) => {
+    if (topic == BEDROOM_SHELF_TEMPERATURE_TOPIC) {
+        console.log("temperature: " + message)
+    }
+    else if (topic == BEDROOM_SHELF_RELATIVE_HUMIDITY_TOPIC) {
+        console.log("humidity: " + message)
+    }
+
+})
